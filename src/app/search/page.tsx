5cfbc0e0ffceb3785d3f848ae68bc6ee961b7651
parkyejin <uiop5809@naver.com>
2024-08-27 09:40:42 +0900

    feat(#55): 검색 결과 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -11,11 +11,20 @@ import { useState, useEffect } from 'react'
 import Button from '@/components/common/Button'
 import { useQueryClient } from '@tanstack/react-query'
 import { queryKeys } from '@/service/search/SearchQueries'
+import Board from '@/components/board/Board'
+import { KeywordSearch } from '@/model/Search'
+import WorryBoard from '@/components/worry/WorryBoard'
+import DiscussionBoard from '@/components/discussion/DiscussionBoard'
+import Container from '@/components/common/Container'
+import { BoardI } from '@/model/Board'
+import { WorryI } from '@/model/Worry'
+import { DiscussionBoardI } from '@/model/Discussion'
 
 const SearchPage = () => {
   const router = useRouter()
   const queryClient = useQueryClient()
   const [keyword, setKeyword] = useState('')
+  const [result, setResult] = useState<KeywordSearch | null>(null)
 
   const { mutate: keywordSearch } = useKeywordSearch()
 
@@ -33,11 +42,12 @@ const SearchPage = () => {
   const handleSearch = () => {
     if (keyword.trim() !== '') {
       keywordSearch(keyword, {
-        onSuccess: () => {
+        onSuccess: (keywordResult) => {
           queryClient.invalidateQueries({ queryKey: queryKeys.recentKeywords })
           queryClient.invalidateQueries({
             queryKey: queryKeys.realtimeKeywords,
           })
+          setResult(keywordResult)
         },
       })
       router.push(`?keyword=${encodeURIComponent(keyword)}`)
@@ -94,28 +104,84 @@ const SearchPage = () => {
         </div>
       </div>
 
-      <div>
-        <div className="text-title3 text-gray1 font-semibold mb-3">
-          인기 검색어
-          <span className="text-caption font-regular text-gray3 ml-2">
-            2023.07.14 18:06 기준
-          </span>
+      {!result && (
+        <div>
+          <div className="text-title3 text-gray1 font-semibold mb-3">
+            인기 검색어
+            <span className="text-caption font-regular text-gray3 ml-2">
+              2023.07.14 18:06 기준
+            </span>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            {Array.isArray(realtimeKeywords) ? (
+              realtimeKeywords.map((item, idx: number) => (
+                <div key={idx} className="flex items-center gap-4">
+                  <span className="text-headline text-maindark font-semibold">
+                    {idx + 1}
+                  </span>
+                  {item.keyword}
+                </div>
+              ))
+            ) : (
+              <div>실시간 인기 검색어를 불러오지 못했습니다.</div>
+            )}
+          </div>
         </div>
-        <div className="flex flex-col gap-3">
-          {Array.isArray(realtimeKeywords) ? (
-            realtimeKeywords.map((item, idx: number) => (
-              <div key={idx} className="flex items-center gap-4">
-                <span className="text-headline text-maindark font-semibold">
-                  {idx + 1}
-                </span>
-                {item.keyword}
-              </div>
-            ))
-          ) : (
-            <div>실시간 인기 검색어를 불러오지 못했습니다.</div>
-          )}
-        </div>
-      </div>
+      )}
+
+      {result && (
+        <>
+          <div>
+            <div className="text-title3 text-maindark font-semibold mb-3">
+              전체 게시판
+            </div>
+            <Container color="purple">
+              {result.boardSimpleInfos.result.length > 0 &&
+                result.boardSimpleInfos.result.map((board: BoardI) => (
+                  <div key={board.id}>
+                    <Board board={board} />
+                    <div className="h-[1px] bg-main my-4 sm:my-6" />
+                  </div>
+                ))}
+            </Container>
+          </div>
+
+          <div>
+            <div className="text-title3 text-maindark font-semibold mb-3">
+              MBTI 과몰입 토론
+            </div>
+            <Container color="purple">
+              {result.getWorriesRes.result.length > 0 &&
+                result.getWorriesRes.result.map((worry: WorryI) => (
+                  <div key={worry.id}>
+                    <WorryBoard worryBoard={worry} />
+                    <div className="h-[1px] bg-main my-4 sm:my-6" />
+                  </div>
+                ))}
+            </Container>
+          </div>
+
+          <div>
+            <div className="text-title3 text-maindark font-semibold mb-3">
+              M쌤 매칭을 기다리는 고민
+            </div>
+            <Container color="purple">
+              {result.discussionSimpleInfo.result.length > 0 &&
+                result.discussionSimpleInfo.result.map(
+                  (discussion: DiscussionBoardI) => (
+                    <div key={discussion.id} className="flex flex-col">
+                      <Container color="purple" className="cursor-pointer">
+                        <DiscussionBoard discussionBoard={discussion} />
+                      </Container>
+                      <div className="h-[1px] bg-gray4 my-2.5 sm:my-12.5" />
+                    </div>
+                  ),
+                )}
+            </Container>
+          </div>
+        </>
+      )}
     </div>
   )
 }
